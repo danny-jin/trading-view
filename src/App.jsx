a7951d89a886685523c85d1878285ea830f12dd8
icarus96119 <icarus96119@gmail.com>
2021-05-24 06:18:01 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import * as React from "react";
-import "./App.css";
-import { BTCUSD } from "./components/trading-view-chart-container/BTCUSD.jsx";
+import React, { useState } from 'react'
+
+import { TradingView } from './components/trading-view-chart-container/trading-view';
+
+import './App.css';
 
 class App extends React.Component {
+
   render() {
     return (
-      <div className={"App"}>
-        <header className={"App-header"}></header>
-        <BTCUSD />
+      <div className={ 'App' }>
+        <header className={ 'App-header' }></header>
+        <TradingView/>
       </div>
     );
   }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
